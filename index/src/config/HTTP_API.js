183ac1424e5780146400ffc3487510f525b0fb64
RNOC1044 <rnoc0144@gmail.com>
2024-12-11 21:02:59 +0700

    แก้ api

--- index/src/config/HTTP_API.js
+++ index/src/config/HTTP_API.js
@@ -1,1 +1,3 @@
-export const SERVER_URL = "http://localhost:5000/api/";
+//export const SERVER_URL = "http://localhost:5000/api/";
+
+export const SERVER_URL = "https://escalation-1.onrender.com/api/";
