356d823de917e9d8e6d2f7bf4475e98905cda470
RNOC1044 <rnoc0144@gmail.com>
2025-01-30 22:04:21 +0700

    http

--- index/src/config/HTTP_API.js
+++ index/src/config/HTTP_API.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "http://localhost:5000/api/";
+//export const SERVER_URL = "http://122.154.58.11:5000/api/";
 
-//export const SERVER_URL = "https://escalation-o1rf.onrender.com";
+export const SERVER_URL = "https://escalation-o1rf.onrender.com/api/";
